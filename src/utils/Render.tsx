e6061db6565a4c5eeffe6cdc4f2908bed8e3fdef
tbolp <tbolp@qq.com>
2024-07-31 16:24:14 +0800

    update package and develop render

--- src/utils/Render.tsx
+++ src/utils/Render.tsx
@@ -3,10 +3,13 @@ export class RenderBuilder {
   private vs_src: string = `#version 300 es
   precision mediump float;
   layout(location = 0) in vec2 pos;
+  layout(location = 1) in vec2 tex;
   out vec2 _pos;
+  out vec2 _tex;
   void main() {
     gl_Position = vec4(pos, 1.0, 1.0);
     _pos = pos;
+    _tex = tex;
   }
   `
   public fs_src: string = `#version 300 es
@@ -23,12 +26,16 @@ export class RenderBuilder {
     let prog = this.create_program()
     let vbo = gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1, -1, 1, -1, 1, 1, -1, -1, -1, 1, 1, 1]), gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-1, -1, 1, -1, 1, 1, -1, -1, -1, 1, 1, 1
+      , 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0
+    ]), gl.STATIC_DRAW)
     let vao = gl.createVertexArray()!
     gl.bindVertexArray(vao)
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
     gl.enableVertexAttribArray(0)
     gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0)
+    gl.enableVertexAttribArray(1)
+    gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 0, 6 * 4 * 2)
     gl.bindVertexArray(null)
     gl.bindBuffer(gl.ARRAY_BUFFER, null)
     return new Render(prog, vao, gl)
@@ -82,5 +89,9 @@ export class Render {
       val.a, val.c, val.e,
       val.b, val.d, val.f
     ])
+  }
+
+  get program() {
+    return this.prog
   }
 }
