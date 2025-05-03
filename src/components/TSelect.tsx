9f2b7e3c9d9c5bb13a1daed4dc6ef6027a5fc8ff
tbolp <tbolp@qq.com>
2025-05-03 10:37:14 +0800

    feat: add TSelect2

--- src/components/TSelect.tsx
+++ src/components/TSelect.tsx
@@ -1,7 +1,9 @@
-import { Grid, Select, SelectProps } from "@mui/material";
-import { ReactNode } from "react";
+import { Grid, MenuItem, Select, SelectProps } from "@mui/material";
+import { ReactNode, useState } from "react";
 
-
+/**
+ * @deprecated
+ */
 export default function TSelect(props: {
   text: string,
   disabled?: boolean,
@@ -18,6 +20,47 @@ export default function TSelect(props: {
       <Grid item xs={true}>
         <Select disabled={props.disabled} {...props.select_props} >
           {props.children}
+        </Select>
+      </Grid>
+    </Grid>
+  );
+}
+
+export interface TSelectValue {
+  label: string
+  value: number
+}
+
+export interface TSelectProps {
+  text: string,
+  disabled?: boolean,
+  values: TSelectValue[]
+  defaultValue?: number,
+  onSelect?: (value: TSelectValue) => void
+}
+
+export function TSelect2(props: TSelectProps) {
+  let [value, set_value] = useState(props.defaultValue)
+  return (
+    <Grid container style={{
+      alignItems: 'center'
+    }}>
+      <Grid item xs={true}>
+        <p>{props.text}</p>
+      </Grid>
+      <Grid item xs={true}>
+        <Select disabled={props.disabled} value={value} onChange={(e) => {
+          set_value(e.target.value as number)
+          let value = props.values.find((v) => v.value === e.target.value)
+          if (value) {
+            props.onSelect?.(value)
+          }
+        }}>
+          {props.values.map((value, index) => (
+            <MenuItem key={index} value={value.value}>
+              {value.label}
+            </MenuItem>
+          ))}
         </Select>
       </Grid>
     </Grid>
